89e78c36ab6f055fc8d801146a84cafd0871183e
QuangVinh15 <quangvinh15092002@gmail.com>
2023-05-15 09:40:54 +0700

    finish project

--- static/js/cart.js
+++ static/js/cart.js
@@ -34,6 +34,9 @@ function addCookieItem(productId, action) {
             delete cart[productId]
         }
     }
+    console.log("Cart: ", cart)
+    document.cookie = "cart=" + JSON.stringify(cart) + ";domain=;path=/"
+    location.reload()
 }
 
 function updateUserOrder(productId, action) {
